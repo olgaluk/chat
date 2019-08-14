021e4ecbd23a83fb43c8420db4dfc8aa758875e8
Olga Luk <olga_ivanova_86@mail.ru>
2019-08-14 19:30:46 +0300

    feat: add basic page layout

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CHAT</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Section 1</h2>
+        </section>
+      </main>
+      <footer className="App-footer">
+        <h3>footer</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
